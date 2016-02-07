5470e393a7ddecc3d70a34157fdc2f03faf22e39
Izabela Styrylska <izabela.styrylska@gmail.com>
2016-02-07 07:39:45 +0100

    initial project version

---
+++ README.md
@@ -0,0 +1,4 @@
+my_project
+==========
+
+A Symfony project created on February 5, 2016, 7:13 pm.
---
+++ src/Dawpal/TestBundle/DawpalTestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dawpal\TestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class DawpalTestBundle extends Bundle
+{
+}
